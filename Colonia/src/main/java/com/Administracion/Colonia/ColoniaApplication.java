47d20f934c103fb7f23a2c4daa2e311d71c716e5
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-02-22 15:51:00 -0600

    feat: Se cargo nuevo proyecto con su estrucutura base

---
+++ Colonia/src/main/java/com/Administracion/Colonia/ColoniaApplication.java
@@ -0,0 +1,13 @@
+package com.Administracion.Colonia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColoniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColoniaApplication.class, args);
+	}
+
+}
