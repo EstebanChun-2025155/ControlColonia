be2274dd684bbf4854674c50544c1da4d4e3b8ac
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-02-22 15:38:33 -0600

    feat: Se cargo nuevo proyecto con su estrucutura base

---
+++ Colonia/src/test/java/com/Administracion/Colonia/ColoniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Administracion.Colonia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColoniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
