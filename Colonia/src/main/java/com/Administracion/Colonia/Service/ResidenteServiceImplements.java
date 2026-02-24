95b63ea71ddfbd8d4dc817780cdbe6522e99a09e
EstebanChun-2025155 <echun-2025155@kinal.edu.gt>
2026-02-24 11:42:32 -0600

    feat: Se cargo la clase ServiceImplements de la entidad Residente con su estructura basica

---
+++ Colonia/src/main/java/com/Administracion/Colonia/Service/ResidenteServiceImplements.java
@@ -0,0 +1,35 @@
+package com.Administracion.Colonia.Service;
+
+import com.Administracion.Colonia.Entity.Residente;
+
+import java.util.List;
+
+public class ResidenteServiceImplements implements ResidenteServices {
+    private final ResidenteServices residenteServices
+
+
+    @Override
+    public List<Residente> getAllResidente() {
+        return List.of();
+    }
+
+    @Override
+    public Residente getResidenteById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Residente saveResidente(Residente residente) throws RuntimeException {
+        return null;
+    }
+
+    @Override
+    public Residente updateResidente(Integer id, Residente residente) {
+        return null;
+    }
+
+    @Override
+    public void deleteResidente(Integer id) {
+
+    }
+}
