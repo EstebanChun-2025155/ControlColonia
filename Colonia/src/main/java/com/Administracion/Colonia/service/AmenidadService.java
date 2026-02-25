c50aec1429f7d79f045ce3933c8c7342d8a1e3e7
JosueChan-2022047 <jchan-2022047@kinal.edu.gt>
2026-02-25 17:46:01 -0600

    feat: Se creo las clases service con sus validaciones y con la logica del CRUD

---
+++ Colonia/src/main/java/com/Administracion/Colonia/service/AmenidadService.java
@@ -0,0 +1,23 @@
+package com.Administracion.Colonia.service;
+
+import com.Administracion.Colonia.entity.Amenidad;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface AmenidadService {
+
+    List<Amenidad> getAllAmenidad();
+
+    Amenidad getAmenidadByid(Integer id);
+
+    Amenidad getProveedorByid(Integer id);
+
+    Amenidad saveAmenidad(Amenidad amenidad) throws RuntimeException;
+
+    Amenidad updateAmenidad(Integer id, Amenidad amenidad);
+
+    void deleteAmenidad(Integer id);
+}
+
