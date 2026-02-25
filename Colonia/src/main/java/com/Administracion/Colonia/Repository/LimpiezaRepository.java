cc41f709ad8c16ff15077f186d76d0fb35ae583d
wilfred <wdepaz-2025275@kinal.edu.gt>
2026-02-24 21:57:00 -0600

    feat: Se cargo la clase Repository de la entidad Limpieza con una validacion de existencia de datos

---
+++ Colonia/src/main/java/com/Administracion/Colonia/Repository/LimpiezaRepository.java
@@ -0,0 +1,16 @@
+package com.Administracion.Colonia.Repository;
+
+import com.Administracion.Colonia.Entity.Limpieza;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface LimpiezaRepository extends JpaRepository<Limpieza, Integer> {
+    Boolean existsByidLimpiezaAndNombreAndPuestoAndJornadaAndSalarioAndTelefono(
+            String nombre,
+            String puesto,
+            String jornada,
+            Double salario,
+            String telefono
+    );
+}
