7e5e22c85957aeeda51764f5cec94065a6287e93
wilfred <wdepaz-2025275@kinal.edu.gt>
2026-02-24 22:08:43 -0600

    feat: Se cargo la clase Controller de la entidad Limpieza con los métodos Get, Post, Delete y Put

---
+++ Colonia/src/main/java/com/Administracion/Colonia/Controller/LimpiezaController.java
@@ -0,0 +1,62 @@
+package com.Administracion.Colonia.Controller;
+
+import com.Administracion.Colonia.Entity.Limpieza;
+import com.Administracion.Colonia.Service.LimpiezaService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/Limpieza")
+public class LimpiezaController {
+    private final LimpiezaService limpiezaService;
+
+    public LimpiezaController(LimpiezaService limpiezaService){
+        this.limpiezaService = limpiezaService; }
+
+    @GetMapping
+    public List<Limpieza> getAllLimpieza(){ return limpiezaService.getAllLimpieza(); }
+
+    @PostMapping
+    public ResponseEntity<Object> createLimpieza(@Valid @RequestBody Limpieza limpieza, BindingResult br){
+        if (br.hasErrors()){
+            return ResponseEntity.badRequest().body(br.getAllErrors().get(0).getDefaultMessage());
+        }
+        try {
+            Limpieza createLimpieza = limpiezaService.saveLimpieza(limpieza);
+            return new ResponseEntity<>(createLimpieza, HttpStatus.CREATED);
+        }catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteLimpieza(@PathVariable Integer id) {
+        try {
+            if (limpiezaService.getLimpiezaById(id) == null) {
+                return ResponseEntity.status(404).body("No existe el empleado de limpieza");
+            }
+            limpiezaService.deleteLimpieza(id);
+            return ResponseEntity.status(202).build();
+        }catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error al eliminar al empleado de limpieza");
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateLimpieza( @PathVariable Integer id, @RequestBody @Valid Limpieza limpieza, BindingResult br) {
+        if (br.hasErrors()){
+            return ResponseEntity.badRequest().body(br.getAllErrors().get(0).getDefaultMessage());
+        }
+        try {
+            Limpieza actualizado = limpiezaService.updateLimpieza(id, limpieza);
+            return ResponseEntity.ok(actualizado);
+        }catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+}
