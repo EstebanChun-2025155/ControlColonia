77a09b53f950e74fc1ab8a4c3b7742deae5ae5f3
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-02-24 20:40:32 -0600

    feat: Se cargaron validaciones de datos

--- Colonia/src/main/java/com/Administracion/Colonia/Service/ResidenteServiceImplements.java
+++ Colonia/src/main/java/com/Administracion/Colonia/Service/ResidenteServiceImplements.java
@@ -1,35 +1,70 @@
 package com.Administracion.Colonia.Service;
 
 import com.Administracion.Colonia.Entity.Residente;
+import com.Administracion.Colonia.Repository.ResidenteRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ResidenteServiceImplements implements ResidenteServices {
-    private final ResidenteServices residenteServices
+    private final ResidenteRepository residenteRepository;
 
-
-    @Override
-    public List<Residente> getAllResidente() {
-        return List.of();
+    public ResidenteServiceImplements(ResidenteRepository residenteRepository) {
+        this.residenteRepository = residenteRepository;
     }
 
     @Override
-    public Residente getResidenteById(Integer id) {
-        return null;
-    }
+    public List<Residente> getAllResidente() { return residenteRepository.findAll();}
+
+    @Override
+    public Residente getResidenteById(Integer id) { return residenteRepository.getReferenceById(id); }
 
     @Override
     public Residente saveResidente(Residente residente) throws RuntimeException {
-        return null;
+        try {
+            if(residenteRepository.ExistsByNombreResidenteAndDpiResidenteAndTelefonoResidenteAndPosicionAndIdCasa(
+                residente.getNomrbe_residente(),
+                residente.getDpi_residente(),
+                residente.getTelefono_residente(),
+                residente.getPosicion(),
+                residente.getId_Casa())){
+
+                throw new RuntimeException("Ya existe una casa con estos datos");
+            }
+                return residenteRepository.save(residente);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     @Override
     public Residente updateResidente(Integer id, Residente residente) {
-        return null;
+        Residente existingResidente = residenteRepository.findById(id).orElseThrow(()-> new RuntimeException("Esta Casa no existe"));
+
+        if (residenteRepository.ExistsByNombreResidenteAndDpiResidenteAndTelefonoResidenteAndPosicionAndIdCasa(
+                residente.getNomrbe_residente(),
+                residente.getDpi_residente(),
+                residente.getTelefono_residente(),
+                residente.getPosicion(),
+                residente.getId_Casa())){
+            throw new RuntimeException("Ya existe una Casa con estos datos");
+        }
+
+        existingResidente.setNomrbe_residente(residente.getNomrbe_residente());
+        existingResidente.setDpi_residente(residente.getDpi_residente());
+        existingResidente.setTelefono_residente(residente.getTelefono_residente());
+        existingResidente.setPosicion(residente.getPosicion());
+        existingResidente.setId_Casa(residente.getId_Casa());
+
+        return residenteRepository.save(existingResidente);
     }
 
     @Override
     public void deleteResidente(Integer id) {
-
+    if(!residenteRepository.existsById(id)){
+        throw new RuntimeException("Este id no existe");
+        }
+        residenteRepository.deleteById(id);
     }
 }
