3a7270b7761b899f676161fc91346e25fd389609
wdepaz-2025275 <wdepaz-2025275@kinal.edu.gt>
2026-02-25 08:13:42 -0600

    feat: Se agrego el atributo idSeguridad en la clase SeguridadRepository

--- Colonia/src/main/java/com/Administracion/Colonia/Repository/SeguridadRepository.java
+++ Colonia/src/main/java/com/Administracion/Colonia/Repository/SeguridadRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public interface SeguridadRepository extends JpaRepository<Seguridad, Integer> {
     Boolean existsByidSeguridadAndNombreAndPuestoAndJornadaAndSalarioAndTelefono(
+            Integer idSeguridad,
             String nombre,
             String puesto,
             String jornada,
