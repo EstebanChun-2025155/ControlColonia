2aa7f9d98633523e2f5a60f59746aee81fbe3436
JosueChan-2022047 <jchan-2022047@kinal.edu.gt>
2026-03-02 20:20:35 -0600

    fix: Se arreglo la estructura de los metodos post, put, delete, get

--- Colonia/src/main/java/com/Administracion/Colonia/controller/VehiculoController.java
+++ Colonia/src/main/java/com/Administracion/Colonia/controller/VehiculoController.java
@@ -1,81 +1,77 @@
 package com.Administracion.Colonia.controller;
-
 import com.Administracion.Colonia.entity.Vehiculo;
-import com.Administracion.Colonia.repository.VehiculoRepository;
+import com.Administracion.Colonia.service.VehiculoService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
+
 
 @RestController
 @RequestMapping("/api/vehiculos")
 public class VehiculoController {
 
-    private final VehiculoRepository vehiculoRepository;
+    private final VehiculoService vehiculoService;
 
-    public VehiculoController(VehiculoRepository vehiculoRepository){
-        this.vehiculoRepository = vehiculoRepository;
+    public VehiculoController(VehiculoService vehiculoService){
+        this.vehiculoService = vehiculoService;
     }
 
-    // LISTAR TODOS
+
     @GetMapping
     public List<Vehiculo> listarTodos(){
-        return vehiculoRepository.findAll();
+        return vehiculoService.getAllVehiculo();
     }
 
-    // CREAR
+
     @PostMapping
-    public ResponseEntity<Object> crearVehiculo(@Valid @RequestBody Vehiculo vehiculo, BindingResult bindingResult) {
-
-        if (bindingResult.hasErrors()){
-            Map<String, String> errores = new HashMap<>();
-            for (FieldError error : bindingResult.getFieldErrors()) {
-                errores.put(error.getField(),error.getDefaultMessage());
-            }
-            return ResponseEntity.badRequest().body(errores);
+    public ResponseEntity<Object> createVehiculo(@Valid @RequestBody Vehiculo vehiculo, BindingResult br){
+        if (br.hasErrors()){
+            return ResponseEntity.badRequest().body(br.getAllErrors().get(0).getDefaultMessage());
         }
-        Vehiculo nuevo = vehiculoRepository.save(vehiculo);
-        return ResponseEntity.ok(nuevo);
+
+        try {
+            Vehiculo crateVehiculo = vehiculoService.saveVehiculo(vehiculo);
+            return new ResponseEntity<>(crateVehiculo, HttpStatus.CREATED);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
-    // ACTUALIZAR
-    @PutMapping("/{id}")
-    public ResponseEntity<?> actualizarVehiculo(@PathVariable Integer id, @Valid @RequestBody Vehiculo vehiculoActulizado, BindingResult bindingResult){
 
-        if (bindingResult.hasErrors()){
-            Map<String,String> errores = new HashMap<>();
-            for (FieldError error : bindingResult.getFieldErrors()){
-                errores.put(error.getField(), error.getDefaultMessage());
-            }
-            return ResponseEntity.badRequest().body(errores);
-        }
-        return vehiculoRepository.findById(id).map(vehiculo -> {
-
-            vehiculo.setIdVehiculo(vehiculoActulizado.getIdVehiculo());
-            vehiculo.setPlaca(vehiculoActulizado.getPlaca());
-            vehiculo.setMarcaModelo(vehiculoActulizado.getMarcaModelo());
-            vehiculo.setColor(vehiculoActulizado.getColor());
-            vehiculo.setPropietario(vehiculoActulizado.getPropietario());
-
-
-            return ResponseEntity.ok(vehiculoRepository.save(vehiculo));
-
-        }).orElse(ResponseEntity.notFound().build());
-    }
-
-    //ELIMINAR
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> eliminaroVehiculo(@PathVariable Integer id){
-        if(!vehiculoRepository.existsById(id)){
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<Object> deleteVehiculo(@PathVariable Integer id){
+        try {
+            if(vehiculoService.getVehiculoByid(id) == null) {
+                return ResponseEntity.status(404).body("No existe este Vehiculo");
+            }
+            vehiculoService.deleteVehiculo(id);
+            return ResponseEntity.status(202).build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error al eliminar Vehiculo");
         }
-        vehiculoRepository.deleteById(id);
-        return ResponseEntity.ok("Vehiculo eliminado correctamente");
     }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateVehiculo(@PathVariable Integer id, @Valid @RequestBody Vehiculo vehiculo, BindingResult br){
+        if (br.hasErrors()){
+            return ResponseEntity.badRequest().body(br.getAllErrors().get(0).getDefaultMessage());
+        }
+        try {
+            Vehiculo actualizado = vehiculoService.updateVehiculo(id, vehiculo);
+            return ResponseEntity.ok(actualizado);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+
+    }
+
 
 }
